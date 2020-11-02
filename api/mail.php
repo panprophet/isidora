ca863d37c1e365c54baef9b61d8ef729ab8411d6
Igor Vujovic <vujovichigor@gmail.com>
2020-11-02 15:53:28 +0100

    Isidorin mail - kontakt

--- api/mail.php
+++ api/mail.php
@@ -1,15 +1,17 @@
 <?php     
-$to_email = 'vujovichigor@gmail.com';
+$to_email = 'isidora.buneta@gmail.com';
 //$to_email = 'psihoterapeut@isidorabuneta.rs';
 $subject = 'Contact from isidorabuneta.rs';
-$message = 'Website kontaktformular: ' . "\r\n" .
+$message = 'Website kontakt formular: ' . "\r\n" .
 'From: ' . $_POST['email'] . "\r\n" .
-'Ime: ' . $_POST['ime'] . "\r\n" .
-'Naslov: ' . $_POST['naslov'] . "\r\n" .
-'Message: ' . $_POST['message'] . "\r\n" .
+'Ime: ' . $_POST['name'] . "\r\n" .
+'Naslov: ' . $_POST['title'] . "\r\n" .
+'Message: ' . $_POST['message'] . "\r\n" ;
 
-$_POST['message'];
 //$headers = 'info@isidorabuneta.rs';
-$headers = 'Reply-To: ' . $_POST['email'] . "\r\n";
+$headers = 'From: psihoterapeut@isidorabuneta.rs' . "\r\n" .
+    'Reply-To: ' . $_POST['email'] . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 echo mail($to_email,$subject,$message,$headers);
+//echo 'ok?';
 ?>
